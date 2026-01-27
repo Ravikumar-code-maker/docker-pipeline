0d855f815a299a48dab00d2d32ee48661cf79c0e
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-01-27 21:10:56 +0530

    Update App.java

---
+++ sample-app/src/main/java/com/app/App.java
@@ -0,0 +1,10 @@
+package com.org.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Application started successfully!");
+        while(true) {
+            // keep container running
+        }
+    }
+}
